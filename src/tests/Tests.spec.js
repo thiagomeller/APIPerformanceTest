1425966cb0afd791e04a0d5867bbf02cc93df67d
Thiago Meller Casagrande <thiago.1359811@alunosatc.edu.br>
2024-11-28 20:13:05 -0300

    testapi

--- src/tests/Tests.spec.js
+++ src/tests/Tests.spec.js
@@ -1,31 +1,56 @@
 import { htmlReport } from 'https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js';
+import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js';
 import http from 'k6/http';
-import { check, sleep } from 'k6';
-import { Counter, Gauge, Rate, Trend } from 'k6/metrics';
+import { check } from 'k6';
+import { Counter, Rate, Trend } from 'k6/metrics';
 
 export const getContactsDuration = new Trend('get_contacts', true);
-
 export const TrendRTT = new Trend('RTT');
 export const RateContentOK = new Rate('ContentOK');
-export const GaugeContentSize = new Gauge('ContentSize');
 export const CounterErrors = new Counter('Errors');
 
 export const options = {
   thresholds: {
     Errors: ['count<100'],
-    ContentSize: ['value<4000'],
     ContentOK: ['rate>0.95'],
     RTT: ['p(99)<300', 'p(70)<250', 'avg<200', 'med<150', 'min<100'],
     http_req_failed: ['rate<0.01'],
     http_req_duration: ['avg<10000']
   },
-  stages: [{ duration: '1m', target: 1000 }]
+  scenarios: {
+    contacts: {
+      executor: 'ramping-vus',
+      startVUs: 10,
+      stages: [
+        { duration: '15s', target: 15 },
+        { duration: '15s', target: 15 },
+        { duration: '15s', target: 15 },
+        { duration: '15s', target: 30 },
+        { duration: '15s', target: 30 },
+        { duration: '15s', target: 30 },
+        { duration: '15s', target: 60 },
+        { duration: '15s', target: 60 },
+        { duration: '15s', target: 60 },
+        { duration: '15s', target: 120 },
+        { duration: '15s', target: 120 },
+        { duration: '15s', target: 120 },
+        { duration: '15s', target: 180 },
+        { duration: '15s', target: 180 },
+        { duration: '15s', target: 180 },
+        { duration: '15s', target: 240 },
+        { duration: '15s', target: 240 },
+        { duration: '15s', target: 240 },
+        { duration: '15s', target: 300 },
+        { duration: '15s', target: 300 }
+      ]
+    }
+  }
 };
 
 export function handleSummary(data) {
   return {
-    './src/output/index.html': htmlReport(data)
-    // stdout: textSummary(data, { indent: ' ', enableColors: true })
+    './src/output/index.html': htmlReport(data),
+    stdout: textSummary(data, { indent: ' ', enableColors: true })
   };
 }
 
@@ -41,12 +66,10 @@ export default function () {
   const OK = 200;
 
   const res = http.get(`${baseUrl}`, params);
-  console.log(res);
 
   getContactsDuration.add(res.timings.duration);
   TrendRTT.add(res.timings.duration);
   RateContentOK.add(OK);
-  // GaugeContentSize.add(res.value.length);
   CounterErrors.add(!OK);
 
   check(res, {
